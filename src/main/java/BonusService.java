77f2ab07b2db9fb5a8ae3656c14fb4c3f0c3c8db
PolyLogic64 <50244580+PolyLogic64@users.noreply.github.com>
2026-03-13 12:34:32 +0100

    - add test for applying restrictions when the bonus is too high

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,5 +1,11 @@
 public class BonusService {
     public double applyRestrictions(double amount) {
-        return 500;
+        if (amount < 500) {
+            return 500;
+        }
+        if (amount > 2000) {
+            return 2000;
+        }
+        return amount;
     }
 }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,12 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusServiceTest {
+    private BonusService bs;
+
+    @BeforeEach
+    void setup() {
+        bs = new BonusService();
+    }
 
     @Test
     void shouldReturnMinWhenBonusIsTooLow() {
-        BonusService bs = new BonusService();
         assertEquals(500, bs.applyRestrictions(400));
+    }
+
+    @Test
+    void shouldReturnMaxWhenBonusIsTooHigh() {
+        assertEquals(2000, bs.applyRestrictions(2100));
     }
 }
