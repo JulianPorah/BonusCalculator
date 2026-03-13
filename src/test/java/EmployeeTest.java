dfbb564c34b417c48b9257041acdd964b1963077
Julian Porath <Julian.Porath@wolters-rundreisen.de>
2026-03-13 12:26:55 +0100

    - adds test for performancePoints

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,14 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmployeeTest {
+    private Employee employee;
+
+    @BeforeEach
+    void setup() {
+        this.employee = new Employee();
+    }
 
     @Test
     void shouldReturnYearsInCompany() {
-        Employee employee = new Employee();
-        employee.setYearsInCompany(4);
+        this.employee.setYearsInCompany(4);
         assertEquals(4, employee.getYearsInCompany());
+    }
+
+    @Test
+    void shouldReturnPerformancePoints() {
+        this.employee.setPerformancePoints(10);
+        assertEquals(10,employee.getPerformancePoints());
     }
 
 }
