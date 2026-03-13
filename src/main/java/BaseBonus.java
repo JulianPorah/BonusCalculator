411a44b9c2b33385070d74abb12e5842037f3f98
MarvinFendler <marvin.fendler@wolters-rundreisen.de>
2026-03-13 12:29:24 +0100

    added basebonus and bonus

---
+++ src/main/java/BaseBonus.java
@@ -0,0 +1,7 @@
+
+public class BaseBonus implements Bonus{
+    public BaseBonus() {}
+
+    @Override
+    public void calculateBonus(Employee employee) {}
+}
---
+++ src/main/java/Bonus.java
@@ -0,0 +1,3 @@
+public interface Bonus {
+    void calculateBonus(Employee employee);
+}
