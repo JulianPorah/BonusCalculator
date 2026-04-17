c098c1dca339f11bb45d021303b99a8ec95dacf6
Julian Porath <Julian.Porath@wolters-rundreisen.de>
2026-04-17 12:28:02 +0200

    - syntax

--- src/main/java/SeniorityBonus.java
+++ src/main/java/SeniorityBonus.java
@@ -6,6 +6,6 @@ public class SeniorityBonus extends BonusDecorator{
     @Override
     public double calculateBonus(Employee employee) {
         double base = super.calculateBonus(employee);
-        return base+ (employee.getYearsInCompany()*20);
+        return base + (employee.getYearsInCompany()*20);
     }
 }
