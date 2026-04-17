edc8dbea4791ba0357282d6e2eaf423adbb3c144
PolyLogic64 <50244580+PolyLogic64@users.noreply.github.com>
2026-04-17 12:05:35 +0200

    added sad-path for test

--- src/test/java/ProjectsCompletionBonusTest.java
+++ src/test/java/ProjectsCompletionBonusTest.java
@@ -1,17 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProjectsCompletionBonusTest {
+    private BaseBonus base;
+    private ProjectsCompletionBonus pcb;
+
+    @BeforeEach
+    void setup() {
+        base = new BaseBonus();
+        pcb = new ProjectsCompletionBonus(base);
+    }
 
     @Test
     void shouldApplyBonus() {
         Employee julian = new Employee("Julian",4,10,2,18,true);
-        BaseBonus base = new BaseBonus();
-        ProjectsCompletionBonus pcb = new ProjectsCompletionBonus(base);
-        double baseBonus = base.calculateBonus(julian);
-        double withProjectCompletionBonus = pcb.calculateBonus(julian);
+        double baseBonus = this.base.calculateBonus(julian);
+        double withProjectCompletionBonus = this.pcb.calculateBonus(julian);
         double diff = withProjectCompletionBonus - baseBonus;
         assertEquals(200.0, diff);
+    }
+
+    @Test
+    void shouldNotApplyBonus() {
+        Employee julian = new Employee("Julian",4,10,0,18,false);
+        double baseBonus = this.base.calculateBonus(julian);
+        double withProjectCompletionBonus = this.pcb.calculateBonus(julian);
+        double diff = withProjectCompletionBonus - baseBonus;
+        assertEquals(0, diff);
     }
 }
